625b52c3d9a3a4be7387c650713045f1556fdde3
Federico Speltini <fedesp71@gmail.com>
2020-08-30 12:48:51 -0300

    docs agregado

---
+++ docs/precache-manifest.79049c445a4e80927282b67b1c5cc027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70934c5cba690a7fb35531d1cd32ee3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e94aebaf0776a8381974",
+    "url": "/static/css/main.4c3d154c.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94aebaf0776a8381974",
+    "url": "/static/js/main.a8444ee1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
